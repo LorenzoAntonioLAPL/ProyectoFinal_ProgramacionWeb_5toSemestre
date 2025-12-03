9184ba91415f93096b03b331d9a5fe49f2d7e622
Angel Oziel Carrillo Diaz de Leon <al500062@edu.uaa.mx>
2025-12-03 16:03:11 -0600

    Arreglo de errores y añadidas funciones del carrito de compra

--- backend/src/models/carrito.model.js
+++ backend/src/models/carrito.model.js
@@ -2,26 +2,26 @@ import connection from "../database/db.js";
 
 export const findUserById = async (id) => {
   const [rows] = await connection.query(
-    "SELECT * FROM listdeseos WHERE user_id = ?",
+    "SELECT * FROM carrito WHERE user_id = ?",
     [id]
   );
   return rows[0];
 };
 
-export async function createLista(user_id, product_ids) {
+export async function createCarrito(user_id, product_ids, product_num) {
     const [result] = await connection.query(
         //............................................................
-        'INSERT INTO listdeseos (user_id, product_ids) VALUES (?, ?)',
-        [user_id, product_ids]
+        'INSERT INTO carrito (user_id, product_ids, product_num) VALUES (?, ?, ?)',
+        [user_id, product_ids, product_num]
     );
     return result;
 }
 
-export async function updateLista(user_id, product_ids) {
+export async function updateCarrito(user_id, product_ids, product_num) {
     const [result] = await connection.query(
         //............................................................
-        'UPDATE listdeseos SET product_ids = ? WHERE user_id = ?',
-        [product_ids, user_id]
+        'UPDATE carrito SET product_ids = ?, product_num = ? WHERE user_id = ?',
+        [product_ids, product_num, user_id]
     );
     return result.affectedRows;
 }
