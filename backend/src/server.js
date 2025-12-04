f1a93dcc292b8cf54cbb7f5f1bb5c173e641f508
Angel Oziel Carrillo Diaz de Leon <al500062@edu.uaa.mx>
2025-12-04 14:37:03 -0600

    Añadidas rutas para el admin

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,13 +21,16 @@ import { isAdmin } from "./middleware/isAdmin.js";
 
 
 const app = express();
+
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
   origin: "*"
 }));
 
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use("/api/auth", authRoutes);
 
