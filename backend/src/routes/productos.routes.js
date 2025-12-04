12dcb3915b7661d203b97b061713ecde39297811
LorenzoAntonioLAPL <lorenzoopacheco06@gmail.com>
2025-12-03 22:54:13 -0600

    cambios para productos adaptacion y chequeo

--- backend/src/routes/productos.routes.js
+++ backend/src/routes/productos.routes.js
@@ -1,22 +1,25 @@
-import { Router } from "express";
-import { getProductos, 
-  getProductById, 
-  createProduct, 
-  updateProduct, 
-  deleteProduct, 
-  updateVentas, 
-  getProductByCategoria} from "../controllers/productos.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
-import { isAdmin } from "../middleware/isAdmin.js";
+import { Router } from "express"
+import {
+  getProductos,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  updateVentas,
+  getProductByCategoria
+} from "../controllers/productos.controller.js"
 
-const router = Router();
+import { verifyToken } from "../middleware/verifyToken.js"
+import { isAdmin } from "../middleware/isAdmin.js"
 
-router.get("/obtenerProductos", getProductos);
-router.get('/obtenerProducto/:id', getProductById);
-router.post('/registrarProducto', verifyToken, isAdmin, createProduct);
-router.put('/actualizarProducto/:id', verifyToken, isAdmin, updateProduct);
-router.put('/actualizarVentas/:id', verifyToken, updateVentas);
-router.delete('/borrarProducto/:id', verifyToken, isAdmin, deleteProduct);
-router.get('/obtenerProductoCategoria/:categoria', getProductByCategoria);
+const router = Router()
 
-module.exports = router;
+router.get("/obtenerProductos", getProductos)
+router.get("/obtenerProducto/:id", getProductById)
+router.post("/registrarProducto", verifyToken, isAdmin, createProduct)
+router.put("/actualizarProducto/:id", verifyToken, isAdmin, updateProduct)
+router.put("/actualizarVentas/:id", verifyToken, updateVentas)
+router.delete("/borrarProducto/:id", verifyToken, isAdmin, deleteProduct)
+router.get("/obtenerProductoCategoria/:categoria", getProductByCategoria)
+
+export default router
