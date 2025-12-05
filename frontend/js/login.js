3ecdef4cde955cb6db58a6a8cdf9b32445cb6aec
LorenzoAntonioLAPL <lorenzoopacheco06@gmail.com>
2025-12-04 18:07:47 -0600

    modificacion para recuperacion de contraseña en front

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -12,6 +12,8 @@ const nombreInput = document.getElementById("nombre")
 const emailInput = document.getElementById("login")
 const passwordInput = document.getElementById("password")
 const confirmPasswordInput = document.getElementById("confirmPassword")
+const forgotPasswordLink = document.getElementById("forgotPassword")
+
 
 const API_BASE_URL = 'https://proyectofinal-programacionweb-5tosemestre.onrender.com';
 
@@ -148,3 +150,39 @@ logoutBtn.addEventListener("click", () => {
   localStorage.removeItem("usuario");
   location.reload();
 });
+
+forgotPasswordLink.addEventListener("click", async (e) => {
+  e.preventDefault()
+  
+  const email = emailInput.value.trim()
+
+  if (!email) {
+    swal("Error", "Escribe tu correo para recuperar la contraseña", "error")
+    return
+  }
+
+  try {
+    const res = await fetch("https://proyectofinal-programacionweb-5tosemestre.onrender.com/api/auth/forgot-password", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email })
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+      swal("Error", data.msg || "Algo salió mal", "error")
+      return
+    }
+
+    swal(
+      "Listo ✅",
+      "Se envió un enlace de recuperación a tu correo",
+      "success"
+    )
+
+  } catch (error) {
+    console.error(error)
+    swal("Error", "No se pudo conectar al servidor", "error")
+  }
+})
