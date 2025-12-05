8b7c02805ddbd657d2ad043180054c0be8977d67
Its-EliGoBrrrrr <areliedgz5005@gmail.com>
2025-12-05 09:29:09 -0600

    Cambio Fuentes

--- frontend/js/pago.js
+++ frontend/js/pago.js
@@ -56,8 +56,41 @@ document.addEventListener('DOMContentLoaded', async () =>{
     }
 
     // Obtener paises
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/ventas/obtenerPaises`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`,
+            }
+        });
+        const data = await response.json();
+        
+        if (response.ok) {
+            let paisCont = document.createElement("fdrm");
+            paisCont.setAttribute("id","opt-pais");
+
+            // Agregar todos los productos obtenidos por data
+            for(var i=0; i<data.length;i++){
+                paisCont.innerHTML +=
+                `<input type="radio" id="pais${data[i].id}" name="pais-compra" value="${data[i].nombre}">
+                <label for="pais${data[i].id}">${data[i].nombre}</label><br>
+                `;
+            }
+
+            btnSect.append(prodCont);
+
+            Promise.prototype.then(mostrarSubtotal());
+            Promise.prototype.then(mostrarTotal());
+        } else {
+            swal("Error", data.msg || "Hubo un error al cargar los paises", "error");
+        }
+    } catch (error) {
+        console.error('Error: No se pudo conectar con el servidor', error);
+        swal("Error", "No se pudo conectar con el servidor", "error");
+    }
 });
 
+// Mostrar formulario tarjeta
 radTarjeta.addEventListener("change", () => {
     if (radTarjeta.checked) {
         let formTar = document.createElement("form");
@@ -91,3 +124,79 @@ radOxxo.addEventListener("change", () => {
         contTarjeta.classList.remove("a-product-card");
     }
 });
+
+async function mostrarSubtotal() {
+    try {
+        const response = await fetch(`${API_BASE_URL}/api/ventas/obtenerSubTotal`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token')}`,
+            }
+        });
+        const data = await response.json();
+        
+        if (response.ok) {
+            let showSubtotal = document.getElementById("p-subtotal");
+
+            showSubtotal.innerHTML = `Subtotal: $${data.subtotal}`;
+        } else {
+            swal("Error", data.msg || "Hubo un error al cargar los productos", "error");
+        }
+    } catch (error) {
+        console.error('Error: No se pudo conectar con el servidor', error);
+        swal("Error", "No se pudo conectar con el servidor", "error");
+    }
+}
+
+async function mostrarTotal() {
+    let idPais;
+
+    const contPais = document.getElementById("opt-pais");
+    let btnPais = contPais.getElementsByTagName("input");
+
+    console.log(btnPais);
+
+    btnPais.forEach(radio => {
+        if(radio.checked){
+            idPais = radio.getAttribute("id");
+        }
+    });
+
+    // try {
+    //     const response = await fetch(`${API_BASE_URL}/api/ventas/obtenerPrecio`, {
+    //         method: "POST",
+    //         headers: {
+    //             "Authorization": `Bearer ${localStorage.getItem('token')}`,
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             idPais: idPais,
+    //         })
+    //     });
+    //     const data = await response.json();
+        
+    //     if (response.ok) {
+    //         let prodCont = document.createElement("div");
+            
+    //         // Agregar todos los productos obtenidos por data
+    //         for(var i=0; i<data.length;i++){
+    //             prodCont.innerHTML +=
+    //             `<div class="a-product-card">
+    //                 <img src="imagenes/donas.jpg" alt="">
+    //                 <div class="a-product-desc">
+    //                     <h2>${data[i].nombre}</h2>
+    //                 </div>
+    //                 <h3>$${data[i].precio}</h3>
+    //             </div>
+    //             `;
+    //         }
+
+    //         prodSect.append(prodCont);
+    //     } else {
+    //         swal("Error", data.msg || "Hubo un error al cargar los productos", "error");
+    //     }
+    // } catch (error) {
+    //     console.error('Error: No se pudo conectar con el servidor', error);
+    //     swal("Error", "No se pudo conectar con el servidor", "error");
+    // }
+}
