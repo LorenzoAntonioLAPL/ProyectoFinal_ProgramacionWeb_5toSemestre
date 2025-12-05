2bd8282f5bdeb47c8157e633c2fd1722249c96fe
LorenzoAntonioLAPL <lorenzoopacheco06@gmail.com>
2025-12-04 20:44:05 -0600

    cambio borrar routes que no

--- backend/src/routes/correo.routes.js
+++
@@ -1,9 +0,0 @@
-// import { Router } from "express";
-// import { sendMail } from "../controllers/nodemailer.controller.js";
-
-// const router = Router();
-
-// router.post("/subscripcion", sendMail);
-
-
-// export default router;
